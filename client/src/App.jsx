0efee40ed1a9d48204e13238e508c6df6f85c511
jmarusak <jmarusak@users.noreply.github.com>
2025-01-30 13:42:14 -0500

    React bases for browser chat UI

---
+++ client/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <h1>Persona</h1>
+      <p>
+        React Chat UI, in progress... 
+      </p>
+    </>
+  )
+}
+
+export default App
